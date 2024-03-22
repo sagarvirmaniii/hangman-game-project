e0d45f2c324a4588b5d02061c9a6e67206488e7d
sagar <sagarvirmni@gmail.com>
2024-03-23 01:04:53 +0530

    languages

---
+++ js/hangman.js
@@ -0,0 +1,16 @@
+var programming_languages = [
+	"python",
+	"javascript",
+	"mongodb",
+	"json",
+	"java",
+	"html",
+	"css",
+	"c",
+	"csharp",
+	"golang",
+	"kotlin",
+	"php",
+	"sql",
+	"ruby"
+]

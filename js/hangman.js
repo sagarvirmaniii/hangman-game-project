b1a158436cbce00e4efcb830995a80caab4e2384
sagar <sagarvirmni@gmail.com>
2024-03-24 13:09:54 +0530

    1st funtion added

--- js/hangman.js
+++ js/hangman.js
@@ -14,3 +14,27 @@ var programming_languages = [
 	"sql",
 	"ruby"
 ]
+let answer = '';
+let maxWrong = 6;
+let mistakes = 0;
+let guessed = [];
+let wordStatus = null;
+
+function randomWord() {
+  answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
+}
+
+function generateButtons() {
+  let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+    `
+      <button
+        class="btn btn-lg btn-primary m-2"
+        id='` + letter + `'
+        onClick="handleGuess('` + letter + `')"
+      >
+        ` + letter + `
+      </button>
+    `).join('');
+
+  document.getElementById('keyboard').innerHTML = buttonsHTML;
+}
